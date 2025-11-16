67f4b1b391faf122a485fdb30bd8297699a87630
EmannRC <reycaroemanuel@gmail.com>
2025-11-16 18:04:08 -0300

    Updated

---
+++ Assets/Scrips/Events/BossEvent.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using TMPro.Examples;
+using Unity.Cinemachine;
+using UnityEngine;
+
+
+
+public class BossEvent : MonoBehaviour
+{
+    [SerializeField] private SeraphBoss _boss;
+    [SerializeField] private Transform _bossFocus;
+    [SerializeField] private DialogSO _bossDialog;
+    [SerializeField] private DialogManager _dialogManager;
+    [SerializeField] private CinemachineVirtualCameraBase _virtualCamera;
+    [SerializeField] private Transform _playerFocus;
+
+    [SerializeField] private P_Attack _p_Attack;
+    [SerializeField] private P_Movement _p_Movement;
+
+    
+
+    private bool triggered = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!triggered && other.CompareTag("Player"))
+        {
+            
+            //p_Attack.enabled = false;
+            //p_Movement.enabled = false;
+
+            triggered = true;
+            StartCoroutine(BossIntroSequence());
+
+
+        }
+    }
+
+    private IEnumerator BossIntroSequence()
+    {
+
+        _p_Movement.canMove = false;
+        _p_Attack.canAttack = false;
+
+        _virtualCamera.Follow = _bossFocus;
+        _virtualCamera.LookAt = _bossFocus;
+
+        yield return new WaitForSeconds(1f);
+
+        _dialogManager.StartDialogue(_bossDialog);
+
+        yield return new WaitUntil(() => _dialogManager.dialogueBox.activeSelf == false);
+
+        _virtualCamera.Follow = _playerFocus;
+        _virtualCamera.LookAt = _playerFocus;
+
+        _boss.StartBattle();
+
+        _p_Movement.canMove = true;
+        _p_Attack.canAttack = true;
+    }
+}
