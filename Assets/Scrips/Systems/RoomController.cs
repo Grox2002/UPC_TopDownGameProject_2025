570b24444ccf13f6e9f90fa39ea2861bfe98cd8a
EmannRC <reycaroemanuel@gmail.com>
2025-11-19 16:51:22 -0300

    Updated
    Corregidos errores de iluminacion

---
+++ Assets/Scrips/Systems/RoomController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _doors;
+    [SerializeField] private List<RoomSpawner> _spawners;
+
+    private bool _fightStarted;
+
+    //private AudioSource _audioSource;
+
+    //public AudioClip openDoorSound;
+
+    //public AudioClip closeDoorSound;
+
+    //public float volume;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !_fightStarted)
+        {
+            StartCoroutine(StartEncounter());
+        }
+    }
+
+    private IEnumerator StartEncounter()
+    {
+        _fightStarted = true;
+
+        CloseDoors();
+
+        yield return new WaitForSeconds(0.5f);
+
+        // Llamar a todos los spawners
+        foreach (var spawner in _spawners)
+        {
+            spawner.SpawnEnemies();
+        }
+
+        // Esperar hasta que todos los spawners tengan sus enemigos derrotados
+        yield return new WaitUntil(() => AllSpawnersCleared());
+
+        OpenDoors();
+    }
+
+    private bool AllSpawnersCleared()
+    {
+        foreach (var spawner in _spawners)
+        {
+            if (!spawner.AllEnemiesDefeated())
+                return false;
+        }
+        return true;
+    }
+
+    private void CloseDoors()
+    {
+        
+
+        foreach (var puerta in _doors)
+        {
+            puerta.SetActive(true);
+        }
+        Debug.Log("Puertas cerradas, hora de la acción.");
+    }
+
+    private void OpenDoors()
+    {
+
+        foreach (var puerta in _doors)
+        {
+            puerta.SetActive(false);
+        }
+        Debug.Log("Sala despejada.");
+    }
+}
---
+++ Assets/Scrips/Systems/SceneSpawnPoint.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSpawnPoint : MonoBehaviour
+{
+    public string sceneToLoad;
+    public string entryPointID;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerPrefs.SetString("ReturnPortal", entryPointID);
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
