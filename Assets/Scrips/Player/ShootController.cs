8a93795c03296b20634c8ae705815ae277f261b7
EmannRC <reycaroemanuel@gmail.com>
2025-11-19 13:07:10 -0300

    Updated
    -Solucionado problemas de colision con el mapa-

---
+++ Assets/Scrips/Player/ShootController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class ShootController : MonoBehaviour
+{
+    //Variables
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Transform _bowTransform;
+    [SerializeField] private Transform _firePoint;
+    [SerializeField] private float _fireRate = 0.5f;
+    [SerializeField] private float _nextFireTime;
+
+    [SerializeField]  private Animator _animator;
+
+    [SerializeField] private float _shootStaminaCost = 1f;
+    
+    private P_Attack _playerAttack;
+
+    //Metodos--------------------------------------------------------------------------------------------------------------------------
+    private void Start()
+    {
+        gameObject.SetActive(false);
+
+        _playerAttack = GetComponentInParent<P_Attack>();
+
+        _animator = GetComponentInChildren<Animator>();
+
+    }
+
+    private void Update()
+    {
+        if (!gameObject.activeInHierarchy) return;
+        RotateBowTowardsMouse();
+    }
+
+    public void Shoot()
+    {
+        // Evita disparar si está en cooldown o sin stamina
+        if (Time.time < _nextFireTime || !_playerAttack.ConsumeStamina(_shootStaminaCost))
+            return;
+
+        //Asegurar que el arco se rote al mouse ANTES de disparar para evitar el error de la flecha desorientada
+        RotateBowTowardsMouse();
+
+        gameObject.SetActive(true);
+        _animator.SetTrigger("Shoot");
+
+        GameObject arrow = Instantiate(_projectilePrefab, _firePoint.position, _firePoint.rotation);
+        arrow.GetComponent<Arrow>()?.SetDirection(_firePoint.right);
+
+        _nextFireTime = Time.time + _fireRate;
+    }
+
+    //Esto rota el arco hacia donde esta el mouse :v
+    public void RotateBowTowardsMouse()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 dir = (mousePos - _bowTransform.position).normalized;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        _bowTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+     
+    }
+}
+
+
